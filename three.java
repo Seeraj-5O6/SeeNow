8616362558fb4500df40b52dc64ff69edc8f5d18
Seeraj-5O6 <shaikseeraj33@gmail.com>
2023-12-01 13:17:56 +0530

    changes made in three.java  update in hell

--- three.java
+++ three.java
@@ -4,7 +4,7 @@ class three{
 public static void main(String[] args){
 
 
-System.out.println("one,two three........");
+System.out.println("one,two three........soon what the hell is going on there");
 
 }
 }
