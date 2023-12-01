d3a122d1123ebdf20e447a37ac1b999444fbc8e3
Seeraj-5O6 <shaikseeraj33@gmail.com>
2023-12-01 12:43:49 +0530

    this two  java prgm files

---
+++ now.java
@@ -0,0 +1,10 @@
+class now{
+
+
+public static void main(String[] args){
+
+
+System.out.println("system terminates");
+
+}
+}
---
+++ three.java
@@ -0,0 +1,10 @@
+class three{
+
+
+public static void main(String[] args){
+
+
+System.out.println("one,two three........");
+
+}
+}
